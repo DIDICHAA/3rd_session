88fd3d02bdd41f8df86cb0a882de67a2ff96f94d
DIDICHAA <jenny615@dongguk.edu>
2023-05-02 12:49:19 +0900

    4th session

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    weather = models.CharField(max_length=10, default=None, null=True)
+    image =  models.CharField(max_length=100, default=None, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
+def detail(request, id):
+    post = get_object_or_404(Post, pk = id)
+    return render(request, 'main/detail.html', {'blog':blog})
+
 def mainpage(request):
+    Posts = Post.objects.all()
     return render(request, 'main/mainpage.html')
 
 def secondpage(request):
     return render(request, 'main/secondpage.html')
+
+def create(request):
+    new_post = Post()
+    new_post.title = request.POST['title']
+    new_post.wirter = request.POST['writer']
+    new_post.pub_date = timezone.now()
+    new_post.body = request.POST['body']
+
+    new_post.save()
+
+    return redirect('detail', new_blog.id)
+
+def new(request):
+    return render(request, 'main/new.html')
